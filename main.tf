6903d767ce03768fdc38c37604aa6813d660ab66
ArunRKkumar1 <167711872+ArunRKkumar1@users.noreply.github.com>
2025-01-01 21:31:39 +0000

    successfully launched ec2_instance

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "example"{
+          ami = "ami-0fd05997b4dff7aac"
+          instance_type = "t2.micro"
+          subnet_id = "subnet-0399fd0c820cbd269"
+          key_name = "arun123"
+    
+
+    tags = {
+            Name = "arun"
+          }
+}
