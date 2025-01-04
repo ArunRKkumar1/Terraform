e50169e7e00b6505a12eb1024b0396fe3bd47172
ArunRKkumar1 <167711872+ArunRKkumar1@users.noreply.github.com>
2025-01-04 05:27:00 +0000

    using terraform create ec2_intance

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -1,15 +1,14 @@
-provider "aws"{
-    region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "example"{
-          ami = "ami-0fd05997b4dff7aac"
-          instance_type = "t2.micro"
-          subnet_id = "subnet-0399fd0c820cbd269"
-          key_name = "arun123"
-    
+resource "aws_instance" "example" { 
+  ami           = "ami-0fd05997b4dff7aac"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0399fd0c820cbd269"
+  key_name      = "arun123"
 
-    tags = {
-            Name = "arun"
-          }
+  tags = {
+    name = "kharwar"
+  }
 }
