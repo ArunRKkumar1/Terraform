05efa3fc99c25110c1392e24e50a90291eecc34a
ArunRKkumar1 <167711872+ArunRKkumar1@users.noreply.github.com>
2025-01-08 07:32:55 +0000

    variable

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,15 +1,4 @@
-variable "ami_value" {
-    description = "value for ami"
-}
 
-variable "instance_type" {
-    description = "value for instance_type"
-}
-
-variable "subnet_id" {
-    description = "value for the subnet_id"
-
-}
 
 
 
---
+++ Day-3/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+    description = "value for ami"
+}
+
+variable "instance_type" {
+    description = "value for instance_type"
+}
+
+variable "subnet_id" {
+    description = "value for the subnet_id"
+
+}
