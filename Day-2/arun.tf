63b42011eb3917678a318fa3c7de019b64138df5
ArunRKkumar1 <167711872+ArunRKkumar1@users.noreply.github.com>
2025-01-08 07:05:11 +0000

    multiple providers

---
+++ Day-2/arun.tf
@@ -0,0 +1,48 @@
+ # Multiple Providers
+
+
+# You can use multiple providers in one single terraform project. For example,
+
+
+# 1. Create a providers.tf file in the root directory of your Terraform project.
+
+# 2. In the providers.tf file, define the AWS and Azure providers. For example:
+
+# why we need provider
+
+#In Terraform, the provider block tells Terraform which cloud platform (like AWS, Azure, or Google Cloud)
+ #to use for creating and managing resources. It acts as a connection between Terraform and the cloud provider,
+  #letting Terraform know where to set up your servers or other services. For example, in AWS,
+   #the provider block includes details like the region to use, so Terraform knows exactly where to create the resources.
+
+# Here we are define aws provider.  Only provider resource after azure.
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+
+# Here we define azure provider. 
+
+provider "azurerm" {
+  subscription_id = "your-azure-subscription-id"
+  client_id = "your-azure-client-id"
+  client_secret = "your-azure-client-secret"
+  tenant_id = "your-azure-tenant-id"
+}
+
+
+# 3. In your other Terraform configuration files, you can then use the aws and azurerm
+#    providers to create resources in AWS and Azure, respectively,
+
+
+resource "aws_instance" "example" {
+    ami = "ami-0fd05997b4dff7aac"
+    subnet_id = "subnet-0399fd0c820cbd269"
+    key_name = "arun123"
+    instance_type = "t2.micro"
+
+    tags = {
+        name = "kumar"
+    }
+}
